ce766b8c32cb01635da92c1b2d597cde3fe1d33b
FjernForlorn <105112725+FjernForlorn@users.noreply.github.com>
2022-12-05 14:42:46 +0300

    Delete 2ch-web-app directory

--- 2ch-web-app/src/main/java/com/fjern/app/persistence/repositories/PrivilegeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fjern.app.persistence.repositories;
-
-import com.fjern.app.persistence.entities.Privilege;
-import com.fjern.common.interfaces.ByNameApi;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrivilegeRepository extends JpaRepository<Privilege, Long>, ByNameApi<Privilege> {
-}
--- 2ch-web-app/src/main/java/com/fjern/app/utils/fileUtils.java
+++
@@ -1,9 +0,0 @@
-package com.fjern.app.utils;
-
-public class fileUtils {
-
-    public static String removeFileExt(String fileName){
-        int indexOfExt=fileName.lastIndexOf('.');
-        return fileName.substring(0, indexOfExt);
-    }
-}
